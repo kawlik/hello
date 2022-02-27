1c5c99ab89cb61ec1c20b0914d30ec1bc526eb9f
kawlik <kpawlik1103@gmail.com>
2022-02-27 21:43:50 +0100

    Login component update

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -26,11 +26,41 @@ export default function Login() {
 /*   *   *   *   *   *   *   *   *   *   */
 
 return(
-    <section id='login'>
+    <section id='login' className='shadow'>
+    <div className='modal-login'>
 
-        <button className='btn btn-outline-primary' onClick={ processLoginWithGoogle }>Login with Google</button>
+        <h2 class='fw-bold fs-1 p-3 pb-2'>
+            Join for free!
+        </h2>
 
-        <button className='btn btn-outline-secondary' onClick={ () => console.log( user ) }>Log</button>
+        <hr className='mb-4'/>
+        
+        <h3 class='fw-bold fs-4'>
+            Use a third-party
+        </h3>
 
+        <button className='w-100 btn btn-outline-primary p-2 my-2' disabled>
+            Join with Facebook
+        </button>
+
+        <button className='w-100 btn btn-outline-success p-2 my-2' onClick={ processLoginWithGoogle }>
+            Join with Google
+        </button>
+
+        <button className='w-100 btn btn-outline-secondary p-2 my-2' disabled>
+            Join with GitHub
+        </button>
+
+        <hr className='mb-4'/>
+        
+        <h3 class='fw-bold fs-4'>
+            Or join as anonymous
+        </h3>
+
+        <button className='w-100 btn btn-outline-dark p-2 my-2' disabled>
+            Join anonymously
+        </button>
+
+    </div>
     </section>
 )};
